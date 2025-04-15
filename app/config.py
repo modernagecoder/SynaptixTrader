bfcf11ed035a15eb4e2c34a0519eaf2cd96c7e1e
Raghav-modernAgeCoder <147975758+Raghav-modernAgeCoder@users.noreply.github.com>
2025-04-15 22:42:22 +0530

    wow test

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@
 
 ACCESS_TOKEN = "<YOUR_ACCESS_TOKEN>"
 SYMBOL = "NSE:SBIN"
-RISK_MANAGEMENT_THRESHOLD = -100  # Adjust risk threshold as needed
+RISK_MANAGEMENT_THRESHOLD = -101  # Adjust risk threshold as needed
 LIVE_TRADING = False
