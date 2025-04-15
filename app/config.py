431986aaa9824e9d0b01faae563276d3ad29fb2f
vansh2064 <vanshagarwal.2064@gmail.com>
2025-04-15 22:49:06 +0530

    FinalCommitBYRaghav

--- app/config.py
+++ app/config.py
@@ -2,5 +2,6 @@
 
 ACCESS_TOKEN = "<YOUR_ACCESS_TOKEN>"
 SYMBOL = "NSE:SBIN"
-RISK_MANAGEMENT_THRESHOLD = -100  # Adjust risk threshold as needed
-LIVE_TRADING = False
+RISK_MANAGEMENT_THRESHOLD = -200  # Adjust risk threshold as needed
+LIVE_TRADING = False 
+
